aef03aaad1e4a87f38a0679aa6ae460eae82e526
SasukeCigano <silvamariasilva301@gmail.com>
2026-04-06 17:07:03 -0300

    Ajustes flake8

--- src/binary_search.py
+++ src/binary_search.py
@@ -5,14 +5,12 @@ def binary_search(array: MyArray, target: int) -> int:
     left = 0
     right = len(array) - 1
     while left <= right:
-        mid = (left+right)//2
+        mid = (left + right)//2
         value = array[mid]
-
         if value == target:
             return mid
         elif value > target:
-            right = mid-1
+            right = mid - 1
         else:
-            left = mid+1
-            
+            left = mid + 1
     return -1
--- src/linear_search.py
+++ src/linear_search.py
@@ -3,6 +3,6 @@ from src.my_array import MyArray
 
 def linear_search(array: MyArray, target: int) -> int:
     for i in range(0, len(array)):
-        if array[i]==target:
+        if array[i] == target:
             return i
     return -1
